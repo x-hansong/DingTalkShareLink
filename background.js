155b8271c7d08e718b8108bb93bb0e281409f2a7
hansong.xhs <hansong.xhs@hansongxhsdeMacBook-Pro.local>
2018-07-23 20:56:22 +0800

    fix config update don't work

--- background.js
+++ background.js
@@ -1,38 +1,40 @@
-var dingTalkMachineApi;
-chrome.storage.sync.get('api_url', function(items) {
-	if(items.api_url) {
-		dingTalkMachineApi = items.api_url;
-	}
-});
-
 function sendToDingTalkGroup(info) {
-	var xhr = new XMLHttpRequest();
-	xhr.onreadystatechange = function() {
-		if(xhr.readyState == 4 && xhr.status == 200) {
-			new Notification('发送结果', {
-				icon: 'dingding.png',
-				body: xhr.responseText
-			});
+	chrome.storage.sync.get('api_url', function (items) {
+		if (items.api_url) {
+			var dingTalkMachineApi;
+			dingTalkMachineApi = items.api_url;
+
+			var xhr = new XMLHttpRequest();
+			xhr.onreadystatechange = function () {
+				if (xhr.readyState == 4 && xhr.status == 200) {
+					new Notification('发送结果', {
+						icon: 'dingding.png',
+						body: xhr.responseText
+					});
+				}
+			};
+			if (!dingTalkMachineApi) {
+				new Notification('发送结果', {
+					icon: 'dingding.png',
+					body: '钉钉机器人api地址未配置，点击选项进行配置'
+				});
+				return;
+			}
+			xhr.open("POST", dingTalkMachineApi, true);
+			xhr.setRequestHeader("Content-Type", "application/json");
+			xhr.send(JSON.stringify(info));
 		}
-	};
-	if(!dingTalkMachineApi) {
-		new Notification('发送结果', {
-			icon: 'dingding.png',
-			body: '钉钉机器人api地址未配置，点击选项进行配置'
-		});
-		return;
-	}
-	xhr.open("POST", dingTalkMachineApi, true);
-	xhr.setRequestHeader("Content-Type", "application/json");
-	xhr.send(JSON.stringify(info));
+	});
+
+
 }
 
-chrome.runtime.onConnect.addListener(function(port) {
+chrome.runtime.onConnect.addListener(function (port) {
 	var tab = port.sender.tab;
 
 	// This will get called by the content script we execute in
 	// the tab as a result of the user pressing the browser action.
-	port.onMessage.addListener(function(info) {
+	port.onMessage.addListener(function (info) {
 		sendToDingTalkGroup(info);
 	});
 });
